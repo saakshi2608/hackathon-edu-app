a712dda13251fe7cbe5b3547a82d97c4af9bdade
saakshi2608 <smath2608@gmail.com>
2024-06-10 01:21:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Title">
+          <div className="Heading">
+            Educational Quizzes
+            <div className= "h1">
+            The concept of holistic development has grown in popularity in recent years, and all educationalists and child development professionals are working hard to promote it. It is well understood that academic achievement alone cannot guarantee a bright and successful life and profession, thus holistic development in early childhood is viewed as quite important.
+What is Holistic Development?
+Holistic development is a complete educational strategy that aims to develop physical, intellectual, emotional, cognitive, and social abilities in kids. It seeks to enhance these skills in the early stage of life, which will prepare them to meet the challenges and difficulties of daily life in the future. These skills also appear crucial not only for success in professional life but also for developing a strong and balanced personality.
+            </div>
+          </div>
+          <div className="subhead">
+            Educational Quizzes for grade 3 and above
+          </div>
+        </div>
+        <div className="Quiz">
+          <div className="card">
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Yoga games
+              </a>
+          </div>
+          <div className="card">
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Nutrition
+            </a>  
+          </div>
+        </div>
       </header>
     </div>
   );
